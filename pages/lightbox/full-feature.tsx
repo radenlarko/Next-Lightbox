25ab8c7e70615cf0ffaaa5b74420e484f19b157c
radenlarko <radenlarko@gmail.com>
2023-02-16 14:19:49 +0700

    Update LightboxFullFeature

--- pages/lightbox/full-feature.tsx
+++ pages/lightbox/full-feature.tsx
@@ -1,11 +1,28 @@
 import Layout from "@/components/Layout";
-import { Button } from "@chakra-ui/react";
+import { Button, useDisclosure } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import React from "react";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import "yet-another-react-lightbox/plugins/captions.css";
+
+const Lightbox = dynamic(() => import("../../components/LightboxComponent"));
 
 const LightboxFullFeature = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Layout title="Lightbox Full Feature">
-      <Button>Open Lightbox</Button>
+      <Button onClick={onOpen}>Open Lightbox</Button>
+      {isOpen ? (
+        <Lightbox
+          open={isOpen}
+          close={onClose}
+          plugins={[Zoom, Fullscreen, Thumbnails, Captions]}
+        />
+      ) : null}
     </Layout>
   );
 };
