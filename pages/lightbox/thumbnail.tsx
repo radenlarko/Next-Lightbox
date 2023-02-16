1406b6179b214ab8535f02d0f8ec385b14099dbe
radenlarko <radenlarko@gmail.com>
2023-02-16 14:08:35 +0700

    Update LightboxThumbnail

--- pages/lightbox/thumbnail.tsx
+++ pages/lightbox/thumbnail.tsx
@@ -1,11 +1,20 @@
 import Layout from "@/components/Layout";
-import { Button } from "@chakra-ui/react";
+import { Button, useDisclosure } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import React from "react";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+const Lightbox = dynamic(() => import("../../components/LightboxComponent"));
 
 const LightboxThumbnail = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Layout title="Lightbox Thumbnail">
-      <Button>Open Lightbox</Button>
+      <Button onClick={onOpen}>Open Lightbox</Button>
+      {isOpen ? (
+        <Lightbox open={isOpen} close={onClose} plugins={[Thumbnails]} />
+      ) : null}
     </Layout>
   );
 };
